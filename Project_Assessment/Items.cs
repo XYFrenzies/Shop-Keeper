9a2fdb0cbf0a56f0fbe7777f061a2c89aa818cbb
Benjamin McDonald <s172834@students.aie.edu.au>
2020-02-20 16:17:56 +1100

    I have gotten started on the interation between the player and the shopkeeper, next step is to make sure it works.

--- Project_Assessment/Items.cs
+++ Project_Assessment/Items.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Project_Assessment
 {
@@ -12,6 +13,7 @@ namespace Project_Assessment
         public string name;
         public int weightOfItem;
         public int costOfItem;
+
         public Items()
         {
             name = "Undefined Item!";
@@ -31,18 +33,55 @@ namespace Project_Assessment
         }
         public virtual void beginningMessage()
         {
-            //This will be the beginning dialogue of the program.
-            Console.WriteLine("Welcome young traveller. How can I help you today?");
-            Console.WriteLine("I have a range of goods that might interest you.");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("The goods that I have available are Armour, weapons of any kind and potions.");
-            Console.WriteLine("Hope I encapsulate your taste of equipment.");
-            Console.WriteLine("");
-            Console.WriteLine("What would you like to look at first?");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("This is the " + Name + " It weighs about " + Weight +"Kg.");
+            int playerMoney = 1000;
+            int shopKeeperMoney = 1000;
+            for (int i = 0; i < 20; i++)
+            {
+
+
+                //This will be the beginning dialogue of the program.
+                Console.WriteLine("Welcome young traveller. How can I help you today?");
+                Console.WriteLine("I have a range of goods that might interest you.");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("The goods that I have available are Armour, weapons of any kind and potions.");
+                Console.WriteLine("Hope I encapsulate your taste of equipment.");
+                Console.WriteLine("");
+                Console.WriteLine("What would you like to look at first?");
+                Console.WriteLine("Options are: ");
+
+
+                //The next statement is for the reading of whatever is in the text file.
+                StreamReader optionsAvailable = new
+                    StreamReader("ShopKeeper.txt");
+                while (optionsAvailable.EndOfStream == false)
+                {
+                    string line = optionsAvailable.ReadLine();
+                    //This is the reading from the text file.
+                    //Whenever the stream is still not at the end, it continually reads it and prints it.
+                    Console.WriteLine(line);
+                }
+                optionsAvailable.Close();
+
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("You have, $" + playerMoney + ".");
+                Console.WriteLine("What would you like to shop for now player or would you like to sell?");
+
+                playerInteraction = Console.ReadLine();
+
+                Program.CheckingForItems();
+
+                Console.WriteLine("Would you like to buy this item? [Y] [N]");
+
+                Program.CheckingYN();
+
+                Program.MoneyCalculator(playerMoney, Cost, shopKeeperMoney);
+                Console.WriteLine("Done, you have " + playerMoney + " left. Would you like to continue? [Y] [N]");
+
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
         }
 
         //The following is used if the value of the variable is changed and called upon, it can be used through their "keywords"
--- Project_Assessment/Program.cs
+++ Project_Assessment/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Data;
 
 namespace Project_Assessment
 {
@@ -15,56 +16,108 @@ namespace Project_Assessment
         static public Armour holyHeadGear = new Armour("The Holy Headset", 15, 500, "HeadGear", 250, "CrossBow");
         static public Armour holyChestPeice = new Armour("The Holy ChestGuard", 25, 900, "ChestPeice", 500, "CrossBow");
         static public Armour holyPants = new Armour("The Holy Leggings", 10, 600, "Leggings", 400, "CrossBow");
-        static public Potion godAlmightyPotion = new Potion("God Almighty Potion", 1, 300, "It gives you health of about ", 200, 
+        static public Potion godAlmightyPotion = new Potion("God Almighty Potion", 1, 300, "It gives you health of about ", 200,
             "Its so strong that it would only be used in a life or death situation.");
-        static public Potion deadlyPoisonOfTheWest = new Potion("Deadly Poison of The West", 2, 1000, 
-            "Deals increasing amounts of damage over 5 seconds, adding x2 damage each time", 20, 
+        static public Potion deadlyPoisonOfTheWest = new Potion("Deadly Poison of The West", 2, 1000,
+            "Deals increasing amounts of damage over 5 seconds, adding x2 damage each time", 20,
             "If you want a deadly toxin that can pressure anyone into fleeing, this is the potion for you.");
         static public Potion theEnchantedPotion = new Potion("The Enchanted Potion of the Neverworlds", 1, 2000,
             "It increases the level of the player by 1", 1,
             "If your needing a high enough level for some of the upcoming fights, this potion should do the trick.");
 
-        Weapon[] weaponsArray = new Weapon[3] { longSwordOfDestiny, deathbringerAxe, crossBow };
-        Armour[] armourArray = new Armour[3] { holyHeadGear, holyChestPeice, holyPants };
-        Potion[] potionArray = new Potion[3] { godAlmightyPotion, deadlyPoisonOfTheWest, theEnchantedPotion };
+        //This is split into two different arrays. One is for the shopkeeper, the other is for the player.
+        static public Items[] itemArrayShopKeeper = new Items[] { longSwordOfDestiny, deathbringerAxe,  
+            holyChestPeice, holyPants, godAlmightyPotion, deadlyPoisonOfTheWest, theEnchantedPotion};
+        static public Items[] itemArrayPlayer = new Items[] { crossBow, holyHeadGear};    
 
-        static void Main()
+        //This is a undefined variable that is used as part of the checking for what the player says.
+        static public string playerInteraction;
+
+        static public void CheckingForItemsInShop(Items[] items)
         {
-            int i;
+
+            //This is asking for a player input for what type of weapon you would need.
+            for (int i = 0; i < 1; i++)
+            {
+
+                if (playerInteraction = items[Name])
+                {
+                    //This is asking that if the players input is the same as the name of the weapon in the array, print out this line.
+                    Console.WriteLine("" + Name + " is worth $" + Cost + " and it weighs " + Weight + ".");
+                    Console.WriteLine("");
+                    i++;
+                }
+                else
+                {
+                    //This is a statement incase the player does not corectly write the correct name of the potion/weapon/armour.
+                    Console.WriteLine("Sorry could you repeat that again?");
+
+                    i = 0;
+                }
+            }
+        }
+
+        static public int MoneyCalculator(int x, int y, int z)
+        {
+            //This quick addition is for when the money is taken from the player, it updates and from minusing from the price.
+            x = x - y;
+            z = z + x;
+            return x;
+
+        }
+
+        static public void CheckingYN()
+        {
+            //This is for checking if the player has input Yes or No.
+            if (playerInteraction = Console.ReadKey("Y"))
+            {
+                
+            }
+            else if (playerInteraction = Console.ReadKey("N"))
+            {
+                
+            }
+
+            else 
+            {
+                Console.WriteLine("Stop Wasting my time, tell me Y or N if you want something!");
+            }
+        }
+
+        static public void Main()
+        {
+
+
             if (!File.Exists("ShopKeeper.txt"))
             {
+                //Within this, it creates a text file called shopkeeper that keeps the inventory of the shopkeeper.
                 StreamWriter shopKeeperValue;
                 shopKeeperValue = new StreamWriter("ShopKeeper.txt");
-                shopKeeperValue.WriteLine(longSwordOfDestiny);
-                shopKeeperValue.WriteLine(deathbringerAxe);
-                shopKeeperValue.WriteLine(crossBow);
-                shopKeeperValue.WriteLine(holyHeadGear);
-                shopKeeperValue.WriteLine(holyChestPeice);
-                shopKeeperValue.WriteLine(holyPants);
-                shopKeeperValue.WriteLine(godAlmightyPotion);
-                shopKeeperValue.WriteLine(deadlyPoisonOfTheWest);
-                shopKeeperValue.WriteLine(theEnchantedPotion);
+                shopKeeperValue.WriteLine("Long Sword Of Destiny");
+                shopKeeperValue.WriteLine("Deathbringer Axe");
+                shopKeeperValue.WriteLine("CrossBow");
+                shopKeeperValue.WriteLine("Holy HeadGear");
+                shopKeeperValue.WriteLine("Holy ChestPeice");
+                shopKeeperValue.WriteLine("Holy Pants");
+                shopKeeperValue.WriteLine("God Almighty Potion");
+                shopKeeperValue.WriteLine("DeadlyPoisonOfTheWest");
+                shopKeeperValue.WriteLine("The Enchanted Potion");
+
+                //DataTable datatable = new DataTable();
+               // StreamReader
+
                 shopKeeperValue.Close();
             }
 
             else if (!File.Exists("Player.txt"))
             {
+                //This is to 
                 StreamWriter playerValue;
                 playerValue = new StreamWriter("Player.txt");
-                playerValue.WriteLine(longSwordOfDestiny);
+                playerValue.WriteLine("Long Sword Of Destiny");
                 playerValue.Close();
 
             }
-
-           /* for (i = 0; i < 100; i++)
-            {
-                Console.ReadLine();
-
-                if( )
-            }/*/
-            //This is the start of the checking of the input. Will take a few days to make sure that it works successfully.
-
-
 
             holyChestPeice.beginningMessage();
             Console.ReadKey();
